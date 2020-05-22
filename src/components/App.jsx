51ebebe510f2b8066d4b3e3429681e1be76428ce
Sherif Abdou <sherif_abdou@outlook.com>
2020-05-21 17:26:19 -0700

    Set up react components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import logo from "./logo.svg";
-import { refreshAsync } from "./redux/actions";
-import store from "./redux/store";
+import { refreshAsync } from "../redux/actions";
+import store from "../redux/store";
 import "./App.css";
 
 function App() {
-  store.dispatch(refreshAsync());
   return <div className="App"></div>;
 }
 
